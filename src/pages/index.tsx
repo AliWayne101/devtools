d3c57c794204dfb4d12df6c72e150832bd9a43ba
AliWayne101 <alimalikwayne@gmail.com>
2023-05-08 01:50:30 +0500

    meta updates

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,11 +5,18 @@ import Integration from '@/sections/Integration'
 import Navbar from '@/sections/Navbar'
 import Setup from '@/sections/Setup'
 import Showcase from '@/sections/Showcase'
+import Head from 'next/head'
 import React from 'react'
 
 const Index = () => {
   return (
     <>
+      <Head>
+        <title>DevTools</title>
+        <meta name="description" content='Looking for an easy way to add notification widgets to your website without any coding skills? Our one-click notification widgets are designed just for you! Our user-friendly interface makes it easy to install and customize your notifications, giving you the power to engage with your audience in a whole new way. Try it today and take your website to the next level!' />
+        <meta name="author" content='Ali Wains' />
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+      </Head>
       <Navbar />
       <main>
         <Hero />
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -5,7 +5,7 @@ const Footer = () => {
         <div className="w-full fira-code text-[var(--slate)]">
             <div className="mt-2">Copyright © 2023 DevTools</div>
             <div className="mt-2">Created by <a href="mailto:alimalikwayne@gmail.com" className="link">Ali A. Wains</a></div>
-            <div className="mt-2"><span className='mr-2'>Cookes</span> Powered by <a href="http://waynedev.vercel.app" target="_blank" className="link">Wayne Development</a></div>
+            <div className="mt-2"><span className='mr-2'>Cookies</span> A project of <a href="http://waynedev.vercel.app" target="_blank" className="link">Wayne Development</a></div>
         </div>
     )
 }
