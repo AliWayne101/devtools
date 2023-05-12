bc6413cd0ecd611cd8be47ee7b277072f6de4f07
AliWayne101 <alimalikwayne@gmail.com>
2023-05-12 16:52:21 +0500

    testing

--- src/pages/api/clients/emailcollector.ts
+++ src/pages/api/clients/emailcollector.ts
@@ -7,5 +7,6 @@ export default async function handler (
     //on post request, check if link has feedback url meanwhile save the data
     
     res.setHeader("Content-Type", "application/javascript");
-    res.status(200).send(`console.log(${req.headers.referer});`);
+    const ref = req.headers.referer;
+    res.status(200).send(`console.log(${ref});`);
 }
