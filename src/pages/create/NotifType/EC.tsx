6eb061c47019e9a6e7ab7a182455200e226fa5d3
AliWayne101 <alimalikwayne@gmail.com>
2023-05-13 17:17:59 +0500

    cleanup v3

--- src/pages/create/NotifType/EC.tsx
+++ src/pages/create/NotifType/EC.tsx
@@ -3,23 +3,25 @@ import React from 'react'
 
 const EC = ({ MountedData, ClosingButton }: { MountedData: ICreatorData, ClosingButton: boolean }) => {
   return (
-    <div className="devToolsBox">
-      <div className='dt_container_head'>
-        <b>{MountedData.notifTitle}</b>
-        {ClosingButton && (
-          <span className='closingButton' title='Close'>x</span>
-        )}
-      </div>
-      <div className='dt_container_body'>
-        <p>{MountedData.notifDesc}</p>
-        <div className='dt_inline_input'>
-          <input type="text" name="name" id="name" placeholder={MountedData.notifNPlaceholder} />
-          <input type="email" name="email" id="email" placeholder={MountedData.notifEPlaceholder} />
+    MountedData && (
+      <div className="devToolsBox">
+        <div className='dt_container_head'>
+          <b>{MountedData.notifTitle}</b>
+          {ClosingButton && (
+            <span className='closingButton' title='Close'>x</span>
+          )}
         </div>
-        <button className='dt_button'>{MountedData.notifButton}</button>
-        <div className='dt_copyright'>Powered by <span className="link cursor-pointer">DevTools</span></div>
+        <div className='dt_container_body'>
+          <p>{MountedData.notifDesc}</p>
+          <div className='dt_inline_input'>
+            <input type="text" name="name" id="name" placeholder={MountedData.notifNPlaceholder} />
+            <input type="email" name="email" id="email" placeholder={MountedData.notifEPlaceholder} />
+          </div>
+          <button className='dt_button'>{MountedData.notifButton}</button>
+          <div className='dt_copyright'>Powered by <span className="link cursor-pointer">DevTools</span></div>
+        </div>
       </div>
-    </div>
+    )
   )
 }
 
