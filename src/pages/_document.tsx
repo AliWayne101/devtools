5b9b1341cd20337c5fa1fcf1cd14253380232e64
AliWayne101 <alimalikwayne@gmail.com>
2023-05-13 17:29:08 +0500

    added metas

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,14 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Unlock the potential of your website with powerful and captivating Notification Widgets. Take your growth to new heights with our expert tools and strategies, designed to skyrocket your conversion rates and attract more customers to your business." />
+        <meta name="author" content="Ali A. Wains" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:title" content='DevTools' />
+        <meta property="og:description" content="Unlock the potential of your website with powerful and captivating Notification Widgets. Take your growth to new heights with our expert tools and strategies, designed to skyrocket your conversion rates and attract more customers to your business." />
+        <meta property="og:image" content="/favicon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
